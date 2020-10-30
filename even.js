0f5087ea2430b5773c6e8f82aed8614ea65dc223
Brage Rosberg <brage70@gmail.com>
2020-10-30 12:49:02 +0100

    Solved even.js

---
+++ even.js
@@ -0,0 +1,8 @@
+/* 
+Accept a random integer within any range, and return "even" or "not even"
+*/
+
+const even = (input) => { 
+  return input % 2 === 0 ? console.log("Even", input) : console.log("Not Even", input); 
+}
+
