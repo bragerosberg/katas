f655c233d0d809ba09d4d7dd9ba9f61290999b0e
Brage Rosberg <brage70@gmail.com>
2020-10-30 12:52:03 +0100

    Added a for loop that generates random integers and tests on the even func

--- even.js
+++ even.js
@@ -6,3 +6,6 @@ const even = (input) => {
   return input % 2 === 0 ? console.log("Even", input) : console.log("Not Even", input); 
 }
 
+for(i = 0; i < 10; i += 1) {
+  even(Math.floor(Math.random() * 1000));
+}
